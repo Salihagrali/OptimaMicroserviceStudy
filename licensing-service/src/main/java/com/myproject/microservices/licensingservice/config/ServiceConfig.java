c0fdcdc6161305d8d5c2c8083d2f22841fcd588a
Salih <imago_999@hotmail.com>
2025-07-28 15:42:17 +0300

    Now docker-compose.yml file executes without any error. When I added the database config into the licensing-service every problem I had resolved instantly.I'll try to do that with my config files only.

--- licensing-service/src/main/java/com/myproject/microservices/licensingservice/config/ServiceConfig.java
+++ licensing-service/src/main/java/com/myproject/microservices/licensingservice/config/ServiceConfig.java
@@ -1,10 +1,12 @@
 package com.myproject.microservices.licensingservice.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "example")
 public class ServiceConfig {
